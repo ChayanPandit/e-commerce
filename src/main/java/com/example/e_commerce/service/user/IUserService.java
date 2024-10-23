42b581eac31a27e4ec0e6ecc61c1d5c2fa26f6d0
ChayanPandit <chayanpandit07@gmail.com>
2024-10-23 20:56:33 +0530

    chore: user

---
+++ src/main/java/com/example/e_commerce/service/user/IUserService.java
@@ -0,0 +1,16 @@
+package com.example.e_commerce.service.user;
+
+import com.example.e_commerce.dto.UserDto;
+import com.example.e_commerce.model.User;
+import com.example.e_commerce.request.CreateUserRequest;
+import com.example.e_commerce.request.UserUpdateRequest;
+
+public interface IUserService {
+
+    User getUserById(Long userId);
+    User createUser(CreateUserRequest request);
+    User updateUser(UserUpdateRequest request, Long userId);
+    void deleteUser(Long userId);
+
+    UserDto convertUserToDto(User user);
+}
