2ba35c5145800ad736db0029c06d2947c5c9cbf4
ChayanPandit <chayanpandit07@gmail.com>
2024-10-23 18:08:47 +0530

    chore: cart repo + services

---
+++ src/main/java/com/example/e_commerce/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package com.example.e_commerce.repository;
+
+import com.example.e_commerce.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Cart findByUserId(Long userId);
+}
---
+++ src/main/java/com/example/e_commerce/service/cart/ICartItemService.java
@@ -0,0 +1,12 @@
+package com.example.e_commerce.service.cart;
+
+import com.example.e_commerce.model.CartItem;
+
+public interface ICartItemService {
+    void addItemToCart(Long cartId, Long productId, int quantity);
+    void removeItemFromCart(Long cartId, Long productId);
+    void updateItemQuantity(Long cartId, Long productId, int quantity);
+
+    CartItem getCartItem(Long cartId, Long productId);
+
+}
